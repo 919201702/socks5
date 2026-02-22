435c4549b85c67201820f96d72bc5e5115ec7413
j jiang <919201702@qq.com>
2026-02-22 11:16:51 +0800

    调整

--- client/src/main/java/com/itjiang/HttpsProxyClient.java
+++
@@ -1,7 +0,0 @@
-package com.itjiang;
-
-public class HttpsProxyClient {
-    public static void main(String[] args) throws InterruptedException {
-        HttpProxyClient.start(true, Config.CLIENT_HTTPS_PORT);
-    }
-}
